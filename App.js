9912bd036c24c5c8e97f08e30a995c23c2656355
eswarmanda <eswar95manda@gmail.com>
2023-07-11 11:07:42 +0000

    Reusable-Banners-React

---
+++ App.js
@@ -0,0 +1,39 @@
+import Banners from './components/BannerCardItem'
+
+import './App.css'
+
+const bannerCardsList = [
+  {
+    id: 1,
+    headerText: 'The Seasons Latest',
+    description: 'Get the seasons all latest designs in a flick of your hand',
+    className: 'card-1',
+  },
+  {
+    id: 2,
+    headerText: 'Our New Designs',
+    description:
+      'Get the designs developed by our in-house team all for yourself',
+    className: 'card-2',
+  },
+  {
+    id: 3,
+    headerText: 'Insiders',
+    description: 'Get the top class products for yourself with an extra off',
+    className: 'card-3',
+  },
+]
+
+const App = () => (
+  <div className="cards-cont">
+    <div>
+      <ul>
+        {bannerCardsList.map(eachItem => (
+          <Banners bannerCards={eachItem} key={eachItem.id} />
+        ))}
+      </ul>
+    </div>
+  </div>
+)
+
+export default App
